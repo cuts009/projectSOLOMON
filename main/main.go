f7c4e3857082de8bdd6abf272415c15e9aa8a3b5
cuts009 <cuts009gamer@gmail.com>
2023-01-24 16:35:24 -0700

    On branch master Changes to be committed: modified:   main/main.go

--- main/main.go
+++ main/main.go
@@ -1,12 +1,12 @@
 package main
 
 //"database/sql"
-
+"fmt"
 //"log"
 //_ "github.com/mattn/go-sqlite3"
 
 func main() {
-	//fmt.Println("\nMain Fucntion Start\n")
+	fmt.Println("\nMain Fucntion Start\n")
 
-	//fmt.Println("\nMain Fucntion End\n")
+	fmt.Println("\nMain Fucntion End\n")
 }
