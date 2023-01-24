a17fe58ade445903cf4436f05a8b0d6d507a311c
cuts009 <cuts009gamer@gmail.com>
2023-01-24 16:32:46 -0700

    Initializing Main Function On branch master Initial commit Changes to be committed: new file:   main/main.go

---
+++ main/main.go
@@ -0,0 +1,12 @@
+package main
+
+//"database/sql"
+
+//"log"
+//_ "github.com/mattn/go-sqlite3"
+
+func main() {
+	//fmt.Println("\nMain Fucntion Start\n")
+
+	//fmt.Println("\nMain Fucntion End\n")
+}
