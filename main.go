d00212cb14c4b1e780f31cfa3022a47283689286
cuts009 <cuts009gamer@gmail.com>
2023-01-30 08:08:19 -0700

    Changed function name

--- main.go
+++ main.go
@@ -11,12 +11,12 @@ import (
 func main() {
 	fmt.Println("Main Fucntion Start")
 
-	processRespond()
+	listenSpeak()
 
 	fmt.Println("Main Fucntion End")
 }
 
-func processRespond() {
+func listenSpeak() {
 	fmt.Println("Process-Respond Fucntion Start")
 
 	ears.Listen()
