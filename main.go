d2cad704bebde7b697bea41f1acc6aa484deae5c
cuts009 <cuts009gamer@gmail.com>
2023-01-30 08:33:49 -0700

    Added startup and idle func

--- main.go
+++ main.go
@@ -11,9 +11,28 @@ import (
 func main() {
 	fmt.Println("Main Fucntion Start")
 
+	startUp()
+
 	listenSpeak()
 
 	fmt.Println("Main Fucntion End")
+}
+
+func startUp() {
+	fmt.Println("Start-Up Function Start")
+
+	fmt.Println("Hello, I am SOLOMON. How can I help you today?")
+
+	idle()
+
+	fmt.Println("Start-Up Function End")
+
+}
+
+func idle() {
+	fmt.Println("Idle Function Start")
+
+	fmt.Println("Idle Function End")
 }
 
 func listenSpeak() {
