811996cf98731c9600e9ebac3b3493d9dd91b9ea
cuts009 <cuts009gamer@gmail.com>
2023-02-01 01:33:04 -0700

    Added Copyright and License Notice

--- cerebrum/cerebrum.go
+++ cerebrum/cerebrum.go
@@ -1,3 +1,7 @@
+// Copyright (C) 2023, Cutter Love (cuts009dev@proton.me)
+// Licensed under the GPL v3.0, you may obtain a copy of this license at:
+// https://www.gnu.org/licenses/gpl-3.0-standalone.html
+
 package cerebrum
 
 import (
